bbc93cc6c603fe740babf654990c3b2418579b9f
akshita-cs <akshitalatex@gmail.com>
2026-03-26 12:03:47 +0530

    Updated Loan eligiility program

---
+++ NestedIf.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class NestedIf {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter age: ");
+        int age = sc.nextInt();
+        System.out.println("Age of the person is: "+age);
+        if(age >= 18){
+            if(age >= 65){
+                System.out.println("Eligible to vote and avail government fund.");
+            }
+            else{
+                System.out.println("Not eligible to vote and avail government fund.");
+            }
+        }
+        else{
+            System.out.println("Not eligible to vote.");
+        }           
+    }
+}
